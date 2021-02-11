1dd745c60b980ca3d1914090da7661c6a15ed82d
Paul Gessinger <hello@paulgessinger.com>
2021-02-11 12:18:07 +0100

    no pre-commit then

--- src/headwind/executor.pyi
+++ src/headwind/executor.pyi
@@ -4,15 +4,11 @@ from typing import Callable, Any, Iterator, TypeVar, TYPE_CHECKING
 
 _T = TypeVar("_T")
 
-
 class SerialExecutor(Executor):
-  def __init__(self) -> None:
-    ...
-
-  def submit(self, __fn: Callable[..., _T], *args: Any, **kwargs: Any) -> Future[_T]:
-    ...
-
+    def __init__(self) -> None: ...
+    def submit(
+        self, __fn: Callable[..., _T], *args: Any, **kwargs: Any
+    ) -> Future[_T]: ...
 
 @contextlib.contextmanager
-def make_executor(jobs: int) -> Iterator[Executor]:
-  ...
+def make_executor(jobs: int) -> Iterator[Executor]: ...
